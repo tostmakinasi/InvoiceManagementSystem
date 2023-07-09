356b5abb7f8d950af91ac5ec22e32eaf9d5ab293
Kubilay Cem KARAHAN <kubilaycem@yandex.com>
2023-07-09 03:37:50 +0300

    Repository -- Add InvoiceType Seed

---
+++ InvoiceManagement.Repository/Seeds/InvoiceTypeSeed.cs
@@ -0,0 +1,25 @@
+﻿using InvoiceManagement.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceManagement.Repository.Seeds
+{
+    internal class InvoiceTypeSeed : IEntityTypeConfiguration<InvoiceType>
+    {
+        public void Configure(EntityTypeBuilder<InvoiceType> builder)
+        {
+            builder.HasData(
+                new InvoiceType { Id = 1, Name = "Aidat" }, 
+                new InvoiceType { Id = 2, Name = "Doğal Gaz" }, 
+                new InvoiceType { Id = 3, Name = "Elektirik" }, 
+                new InvoiceType { Id = 4, Name = "Su" }
+                );
+        }
+    }
+}
