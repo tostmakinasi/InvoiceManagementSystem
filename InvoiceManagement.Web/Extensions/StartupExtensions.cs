9a0175a48e92192552753f81189c492152b2102d
Kubilay Cem KARAHAN <kubilaycem@yandex.com>
2023-11-12 00:31:47 +0300

    Web -- Added Cookie, SecurityStamp Options

--- InvoiceManagement.Web/Extensions/StartupExtensions.cs
+++ InvoiceManagement.Web/Extensions/StartupExtensions.cs
@@ -2,6 +2,7 @@
 using InvoiceManagement.Core.Repositories;
 using InvoiceManagement.Core.Services;
 using InvoiceManagement.Core.UnitOfWorks;
+using InvoiceManagement.Repository;
 using InvoiceManagement.Repository.Repositories;
 using InvoiceManagement.Repository.UnitOfWorks;
 using InvoiceManagement.Services.Services;
@@ -20,12 +21,18 @@ namespace InvoiceManagement.Web.Extensions
             services.AddScoped<IApartmentService, ApartmentService>();
             services.AddScoped<IHouseTypeService, HouseTypeService>();
             services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IEmailService, EmailService>();
         }
         public static void AddIdentityWithExtension(this IServiceCollection services)
         {
             services.Configure<DataProtectionTokenProviderOptions>(opt =>
             {
                 opt.TokenLifespan = TimeSpan.FromHours(2);
+            });
+
+            services.Configure<SecurityStampValidatorOptions>(opt =>//30 dkda bir veritabanı ile client cookie de bulunan securitystamp değerlerini karşılaştıracak
+            {
+                opt.ValidationInterval = TimeSpan.FromMinutes(30);
             });
 
             services.AddIdentity<User, Role>(options =>
@@ -42,8 +49,23 @@ namespace InvoiceManagement.Web.Extensions
                 options.Lockout.MaxFailedAccessAttempts = 3;
 
 
-            });
+            }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders(); 
         }
 
+        public static void AddCookieWithExtension(this IServiceCollection services)
+        {
+            services.ConfigureApplicationCookie(opt =>
+            {
+                var cookieBuilder = new CookieBuilder();
+                cookieBuilder.Name = "InvoiceManagementSystemCookie";
+                opt.Cookie = cookieBuilder;
+                opt.LogoutPath = new PathString("/Members/Logout");
+                opt.LoginPath = new PathString("/Home/Signin");
+                opt.ExpireTimeSpan = TimeSpan.FromDays(60);
+                opt.SlidingExpiration = true;
+            });
+        }
     }
+
+
 }
